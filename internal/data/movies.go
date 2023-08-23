96473ba0395a88f77b0526ae2e45a2787f6f244a
Piotr Prokopowicz <git@nes.slmail.me>
2023-08-23 11:48:26 +0200

    Movies Update function and update movie handler implemented

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -84,7 +84,21 @@ func (m MovieModel) Get(id int64) (*Movie, error) {
 }
 
 func (m MovieModel) Update(movie *Movie) error {
-	return nil
+	stmt := `
+	UPDATE movies
+	SET title = $1, year = $2, runtime = $3, genres = $4, version = version + 1
+	WHERE id = $5
+	RETURNING version`
+
+	args := []any{
+		movie.Title,
+		movie.Year,
+		movie.Runtime,
+		pq.Array(movie.Genres),
+		movie.ID,
+	}
+
+	return m.DB.QueryRow(stmt, args...).Scan(&movie.Version)
 }
 
 func (m MovieModel) Delete(id int64) error {
