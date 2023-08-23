c723e02dc8623183eecca4d668233b27667fc4ae
Piotr Prokopowicz <git@nes.slmail.me>
2023-08-23 11:56:22 +0200

    Movies Delete function and delete movie handler implemented

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -102,5 +102,25 @@ func (m MovieModel) Update(movie *Movie) error {
 }
 
 func (m MovieModel) Delete(id int64) error {
+	if id < 1 {
+		return ErrRecordNotFound
+	}
+
+	query := `DELETE FROM movies WHERE id = $1`
+
+	result, err := m.DB.Exec(query, id)
+	if err != nil {
+		return err
+	}
+
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return nil
+	}
+
+	if rowsAffected == 0 {
+		return ErrRecordNotFound
+	}
+
 	return nil
 }
