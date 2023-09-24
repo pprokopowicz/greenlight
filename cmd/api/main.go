d8c9c272e1368bdb6f88f3e02797b90e6dc30f13
Piotr Prokopowicz <git@nes.slmail.me>
2023-09-24 11:56:22 +0200

    Basic email sending functionality implemented

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -8,8 +8,10 @@ import (
 	"os"
 	"time"
 
-	_ "github.com/lib/pq"
 	"github.com/pprokopowicz/greenlight/internal/data"
+	"github.com/pprokopowicz/greenlight/internal/mailer"
+
+	_ "github.com/lib/pq"
 )
 
 const version = "1.0.0"
@@ -28,12 +30,20 @@ type config struct {
 		burst   int
 		enabled bool
 	}
+	smtp struct {
+		host     string
+		port     int
+		username string
+		password string
+		sender   string
+	}
 }
 
 type application struct {
 	config config
 	logger *slog.Logger
 	models data.Models
+	mailer mailer.Mailer
 }
 
 func main() {
@@ -51,6 +61,12 @@ func main() {
 	flag.Float64Var(&cfg.limiter.rps, "limiter-rps", 2, "Rate limiter maximum requests per second")
 	flag.IntVar(&cfg.limiter.burst, "limiter-burst", 4, "Rate limiter maximum burst")
 	flag.BoolVar(&cfg.limiter.enabled, "limiter-enabled", true, "Enable rate limiter")
+
+	flag.StringVar(&cfg.smtp.host, "smtp-host", "sandbox.smtp.mailtrap.io", "SMTP host")
+	flag.IntVar(&cfg.smtp.port, "smtp-port", 25, "SMTP port")
+	flag.StringVar(&cfg.smtp.username, "smtp-username", os.Getenv("SMTP_USERNAME"), "SMTP username")
+	flag.StringVar(&cfg.smtp.password, "smtp-password", os.Getenv("SMTP_PASSWORD"), "SMTP password")
+	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "Greenlight <no-reply@greenlight.pprokopowicz.net>", "SMTP sender")
 
 	flag.Parse()
 
@@ -70,6 +86,7 @@ func main() {
 		config: cfg,
 		logger: logger,
 		models: data.NewModels(db),
+		mailer: mailer.New(cfg.smtp.host, cfg.smtp.port, cfg.smtp.username, cfg.smtp.password, cfg.smtp.sender),
 	}
 
 	err = app.serve()
