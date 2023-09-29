edc3dec0e784b8d262f2639946c9d7040a054ae6
Piotr Prokopowicz <git@nes.slmail.me>
2023-09-29 20:52:13 +0200

    realip used in rateLimit middleware

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -4,7 +4,6 @@ import (
 	"errors"
 	"expvar"
 	"fmt"
-	"net"
 	"net/http"
 	"strconv"
 	"strings"
@@ -13,6 +12,8 @@ import (
 
 	"github.com/pprokopowicz/greenlight/internal/data"
 	"github.com/pprokopowicz/greenlight/internal/validator"
+
+	"github.com/tomasen/realip"
 	"golang.org/x/time/rate"
 )
 
@@ -54,11 +55,7 @@ func (app *application) rateLimit(next http.Handler) http.Handler {
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if app.config.limiter.enabled {
-			ip, _, err := net.SplitHostPort(r.RemoteAddr)
-			if err != nil {
-				app.serverErrorResponse(w, r, err)
-				return
-			}
+			ip := realip.FromRequest(r)
 
 			mu.Lock()
 
